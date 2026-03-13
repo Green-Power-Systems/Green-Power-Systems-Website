f7188c022dc223b4b0d1b2efcf707b5197a73dc5
Muse-feta <fetamuse@gmail.com>
2026-03-13 19:25:26 +0300

    try with use dynamic

--- app/page.tsx
+++ app/page.tsx
@@ -1,37 +1,34 @@
-import About from '@/components/About'
-import CompanyAbout from '@/components/about/CompanyAbout';
-import Blog from '@/components/Blog';
-import CTA from '@/components/CTA';
-import Facts from '@/components/Facts';
-import Feedback from '@/components/Feedback';
-import Gallery from '@/components/Gallery';
-import GapTop from '@/components/GapTop';
 import Hero from '@/components/Hero'
-import Projects from '@/components/Projects';
-import Service from '@/components/Service';
-import Team from '@/components/Team';
-import WhyChoose from '@/components/WhyChoose';
-import Script from 'next/script';
-import React from 'react'
+import About from '@/components/About'
+
+
+import dynamic from "next/dynamic"
+
+const CompanyAbout = dynamic(() => import("@/components/about/CompanyAbout"));
+const WhyChoose = dynamic(() => import("@/components/WhyChoose"));
+const CTA = dynamic(() => import("@/components/CTA"));
+const Facts = dynamic(() => import('@/components/Facts'))
+const Service = dynamic(() => import('@/components/Service'))
+const Gallery = dynamic(() => import('@/components/Gallery'))
+const Feedback = dynamic(() => import('@/components/Feedback'))
+const Blog = dynamic(() => import('@/components/Blog'))
 
 const page = () => {
   return (
     <div>
       <Hero />
       <About />
-      <CompanyAbout/>
-      <WhyChoose/>
-      <CTA/>
-      <Facts/>
-      <Service/>
-      <Gallery/>
-      <Feedback/>
-      {/* <Projects/> */}
-       {/* <GapTop/>*/}
-      {/* <Team/> */}
-      <Blog/>
+      <CompanyAbout />
+      <WhyChoose />
+      <CTA />
+
+      <Facts />
+      <Service />
+      <Gallery />
+      <Feedback />
+      <Blog />
     </div>
-  );
+  )
 }
 
 export default page
